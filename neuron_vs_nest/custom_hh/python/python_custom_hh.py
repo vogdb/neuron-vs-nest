72b63dfe77eee0f580da0145025ab6ca4d3b087c
vogdb <vozhdb@gmail.com>
2018-01-15 14:35:20 +0300

    draft of working version on pure python

--- neuron_vs_nest/custom_hh/python/python_custom_hh.py
+++ neuron_vs_nest/custom_hh/python/python_custom_hh.py
@@ -16,9 +16,10 @@ E_K  = -80.0
 E_L  = -70.0
 R = 8.314472
 F = 96485.34
+# E_Ca = 40.0
 Temperature = 309.15
 Ca_out=2.0
-Ca_in=0.0001
+Ca_in_init=0.0001
 
 # Channel gating kinetics
 # Functions of membrane voltage
@@ -46,33 +47,32 @@ def I_CaN(V, mc, hc, E_Ca):     return g_CaN * mc**2 * hc * (V - E_Ca)
 #  Calcium L
 def I_CaL(V, p, E_Ca):     return g_CaL * p * (V - E_Ca)
 #  Calcium K
-def I_CaK(V):     return 0.6 * g_CaK * ((Ca_in * Ca_in)/(Ca_in * Ca_in + 0.014 * 0.014))*(V - E_K)
+def I_CaK(V, Ca_in):     return 0.6 * g_CaK * ((Ca_in * Ca_in)/(Ca_in * Ca_in + 0.014 * 0.014))*(V - E_K)
 #  E_Ca
 def E_Ca_func(Ca_in):     return ((1000.0 * R * Temperature)/(2.0 * F))*sp.log(Ca_out/Ca_in)
 # External current
-def I_inj(t): return 700.0*(t < 80)
+def I_inj(t): return 700.0
 
 # The time to integrate over
-t = sp.arange(0.0, common_util.RUN_TIME, 0.1)
+t = sp.arange(0.0, 150.0, 0.1)
 
 # Integrate!
 def dALLdt(X, t):
     V, m, h, n, p, mc, hc, Ca_in = X
     E_Ca = ((1000.0 * R * Temperature)/(2.0 * F))*sp.log(Ca_out/Ca_in)
     #calculate membrane potential & activation variables
-    dVdt = (I_inj(t) - I_Na(V, m, h) - I_K(V, n) - I_L(V) - I_CaN(V, mc, hc, E_Ca) - I_CaL(V, p, E_Ca) - I_CaK(V)) / C_m
+    dVdt = (I_inj(t) - I_Na(V, m, h) - I_K(V, n) - I_L(V) - I_CaN(V, mc, hc, E_Ca) - I_CaL(V, p, E_Ca) - I_CaK(V, Ca_in)) / C_m
     dmdt = alpha_m(V)*(1.0-m) - beta_m(V)*m
     dhdt = (h_inf(V) - h) / h_tau(V)
     dndt = (n_inf(V) - n) / n_tau(V)
     dpdt = (p_inf(V) - p) / p_tau()
     dmcdt = (mc_inf(V) - mc) / mc_tau()
     dhcdt = (hc_inf(V) - hc) / hc_tau()
-    dca_indt = 0.01 * (-(I_CaN(V, mc, hc, E_Ca) + I_CaL(V, p, E_Ca)) - 4.0*Ca_in)
+    dca_indt = 0.01 * (-0.00001 * (I_CaN(V, mc, hc, E_Ca) + I_CaL(V, p, E_Ca)) - 4.0*Ca_in)
     return dVdt, dmdt, dhdt, dndt, dpdt, dmcdt, dhcdt, dca_indt
 
 V_Init = -65
-E_Ca_init = ((1000.0 * R * Temperature)/(2.0 * F))*sp.log(Ca_out/Ca_in)
-X = odeint(dALLdt, [V_Init, alpha_m(V_Init)/(alpha_m(V_Init) + beta_m(V_Init)), h_inf(V_Init), n_inf(V_Init), p_inf(V_Init), mc_inf(V_Init), hc_inf(V_Init), Ca_in], t)
+X = odeint(dALLdt, [V_Init, alpha_m(V_Init)/(alpha_m(V_Init) + beta_m(V_Init)), h_inf(V_Init), n_inf(V_Init), p_inf(V_Init), mc_inf(V_Init), hc_inf(V_Init), Ca_in_init], t)
 V = X[:,0]
 m = X[:,1]
 h = X[:,2]
@@ -85,9 +85,9 @@ ina = I_Na(V,m,h)
 ik = I_K(V, n)
 il = I_L(V)
 E_Ca = E_Ca_func(Ca_in)
-# ican = I_CaN(V,mc,hc)
-# ical = I_CaL(V,p)
-# icak = I_CaK(V,p)
+ican = I_CaN(V,mc,hc, E_Ca)
+ical = I_CaL(V,p, E_Ca)
+icak = I_CaK(V,Ca_in)
 
 
 pylab.figure("python iclamp V_m")
@@ -97,10 +97,14 @@ pylab.subplot(4,1,1)
 pylab.title('Hodgkin-Huxley Neuron')
 pylab.plot(t, V, 'k')
 pylab.ylabel('V (mV)')
+# pylab.plot(t, ican, 'k', label='icaN')
+# pylab.plot(t, icak, 'k', label='icaK')
+pylab.legend()
 
 pylab.subplot(4,1,2)
+# pylab.plot(t, ical, 'c', label='icaL')
+# pylab.ylabel('Ca_in')
 pylab.plot(t, Ca_in, 'c', label='Ca_in')
-pylab.ylabel('Ca_in')
 # pylab.plot(t, ina, 'c', label='$I_{Na}$')
 # pylab.plot(t, ik, 'y', label='$I_{K}$')
 # pylab.plot(t, il, 'm', label='$I_{L}$')
@@ -115,9 +119,9 @@ pylab.ylabel('Gating Value')
 pylab.legend()
 
 pylab.subplot(4,1,4)
-pylab.plot(t, E_Ca, 'k')
-pylab.xlabel('t (ms)')
-pylab.ylabel('E_Ca')
+# pylab.plot(t, E_Ca, 'k')
+# pylab.xlabel('t (ms)')
+# pylab.ylabel('E_Ca')
 # pylab.plot(t, [I_inj(t)] * len(t), 'k')
 # pylab.xlabel('t (ms)')
 # pylab.ylabel('$I_{inj}$ pA')
